d63c6d13643f59e56494916ac7f538a79387e7f4
bortlip <45794448+bortlip@users.noreply.github.com>
2026-08-05 19:11:47 -0400

    Refactor Pocket Prix source modules

---
+++ simulations/pocket-prix/src/js/constants.js
@@ -0,0 +1,12 @@
+// Simulation dimensions and fixed physical constants.
+'use strict';
+
+const WORLD_W = 1280;
+const WORLD_H = 720;
+const TRACK_WIDTH = 92;
+const CAR_LENGTH = 19;
+const CAR_WIDTH = 10;
+const WHEELBASE = 13.5;
+const FIXED_DT = 1 / 120;
+const MAX_STEPS_PER_FRAME = 120;
+const PIT_S = 44;
